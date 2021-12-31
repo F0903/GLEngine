e7a3775b8d30af5c97e93fc22b2367563ee11188
Frederik P <frepbh@gmail.com>
2021-12-31 11:05:32 +0100

    Removed a picture.

--- GLEngine/resources/Test.lua
+++ GLEngine/resources/Test.lua
@@ -1,9 +1,6 @@
 cah = LoadTexture("./resources/cah.png");
-alabama = LoadTexture("./resources/alabama.png");
 
 function Update() -- DO NOT PUT LOADTEXTURE IN UPDATE LOOP WILL CAUSE MEMORY LEAK
 	SetTexture(cah);
-	DrawSquare(0, 0, "50%", "100%");
-	SetTexture(alabama);
-	DrawSquare("100%", 0, "50%", "100%");
+	DrawSquare(0, 0, "100%", "100%");
 end
